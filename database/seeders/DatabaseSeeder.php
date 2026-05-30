a4b75e46c3bfb51692b658fbd1b18947e060391e
gts <gtshadow33@gmail.com>
2026-05-30 22:24:40 +0200

    onli admini make seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,52 +3,18 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Juego;
-use App\Models\Genero;
 use Illuminate\Database\Seeder;
-use Database\Seeders\ComentarioSeeder;
-use Database\Seeders\GeneroSeeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-       
-        User::factory()->create([
+        User::create([
             'name' => 'Admin',
             'email' => 'admin@test.com',
-            'password' => bcrypt('password'),
+            'password' => Hash::make('password'),
             'role' => 'admin',
-        ]);
-
-        //  USERS normales
-        User::factory(5)->create([
-            'role' => 'user',
-        ]);
-
-        //  GENEROS
-        $generos = [
-            'Acción',
-            'Aventura',
-            'RPG',
-            'Deportes',
-            'Terror',
-            'Estrategia',
-            'Shooter'
-        ];
-
-        foreach ($generos as $nombre) {
-            Genero::create([
-                'nombre' => $nombre
-            ]);
-        }
-
-        //  JUEGOS
-        Juego::factory(10)->create();
-
-        //  COMENTARIOS
-        $this->call([
-            ComentarioSeeder::class,
         ]);
     }
 }
