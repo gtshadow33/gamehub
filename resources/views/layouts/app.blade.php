ad488885570fe3f13fefda9b71b595894e52bd7d
gts <gtshadow33@gmail.com>
2026-05-30 22:54:51 +0200

    second

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GameHub</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'], 'build')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-zinc-950 text-white">
